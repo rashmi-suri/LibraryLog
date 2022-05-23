c3725cc55af76b657efe18a62267d3dee83dcd7b
Rashmi Suri <rashmi.suri@avanade.com>
2022-05-23 10:45:11 +0100

    Create an interface for CRUD enabled services in Services directory

---
+++ LibraryLog/Services/ICrudService.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryLog.Services
+{
+    public interface ICrudService<T, U>
+    {
+        // CRUD
+        public IEnumerable<T> GetAll();
+        public T Get(U id);
+        public void Add(T element);
+        public void Update(T oldElement, T newElement);
+        public void Delete(U id);
+    }
+
+}
