6071183768683717f423b3b42d0217a6a2dc92f0
Rashmi Suri <rashmi.suri@avanade.com>
2022-05-23 12:51:27 +0100

    Create database context for the description

---
+++ LibraryLog/Data/DescriptionContext.cs
@@ -0,0 +1,14 @@
+﻿using LibraryLog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryLog.Data
+{
+    public class DescriptionContext : DbContext
+    {
+        public DescriptionContext(DbContextOptions<DescriptionContext> options) : base(options)
+        {
+        }
+        public DbSet<BookDescription> BookDescriptions { get; set; }
+    }
+
+}
