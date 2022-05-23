c58ee059c49515fc5ffbd87b1bde72a5583cd0d7
Rashmi Suri <rashmi.suri@avanade.com>
2022-05-23 12:46:06 +0100

    New class in models folder for the book description

---
+++ LibraryLog/Models/BookDescription.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryLog.Models
+{
+    public class BookDescription
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [MinLength(2, ErrorMessage = "Length of description cannot be less than 2 characters")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        public string? Status { get; set; }
+    }
+
+}
