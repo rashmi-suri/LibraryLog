9de2bed0cfa18e3d5198d90d985adb6a89aaa13f
Rashmi Suri <rashmi.suri@avanade.com>
2022-05-23 13:21:13 +0100

    Add services to the container for book and description

--- LibraryLog/Program.cs
+++ LibraryLog/Program.cs
@@ -1,6 +1,22 @@
+using LibraryLog.Data;
+using LibraryLog.Data.Repositories;
+using LibraryLog.Models;
+using LibraryLog.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container BOOK
+builder.Services.AddDbContext<BookContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICrudRepository<Book, int>, BookRepository>();
+builder.Services.AddScoped<ICrudService<Book, int>, BookService>();
+
+// Add services to the container DESCRIPTION
+builder.Services.AddDbContext<DescriptionContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICrudRepository<BookDescription, int>, DescriptionRepository>();
+builder.Services.AddScoped<ICrudService<BookDescription, int>, DescriptionService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
