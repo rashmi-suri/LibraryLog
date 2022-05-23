7a0e0c3187a5f4554ad55f77ac6739423952e973
Rashmi Suri <rashmi.suri@avanade.com>
2022-05-23 10:55:35 +0100

    Create an implementation of the CRUD service interface for the Book model inside the Services directory

---
+++ LibraryLog/Services/BookService.cs
@@ -0,0 +1,40 @@
+﻿using LibraryLog.Data.Repositories;
+using LibraryLog.Models;
+
+namespace LibraryLog.Services
+{
+    public class BookService : ICrudService<Book, int>
+    {
+        private readonly ICrudRepository<Book, int> _BookRepository;
+        public BookService(ICrudRepository<Book, int> BookRepository)
+        {
+            _BookRepository = BookRepository;
+        }
+        public void Add(Book element)
+        {
+            _BookRepository.Add(element);
+            _BookRepository.Save();
+        }
+        public void Delete(int id)
+        {
+            _BookRepository.Delete(id);
+            _BookRepository.Save();
+        }
+        public Book Get(int id)
+        {
+            return _BookRepository.Get(id);
+        }
+        public IEnumerable<Book> GetAll()
+        {
+            return _BookRepository.GetAll();
+        }
+        public void Update(Book old, Book newT)
+        {
+            old.Description = newT.Description;
+            old.Status = newT.Status;
+            _BookRepository.Update(old);
+            _BookRepository.Save();
+        }
+    }
+
+}
