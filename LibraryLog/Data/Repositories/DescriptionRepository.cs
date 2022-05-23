017007cf677d03b0e661fe495bc37b8efb54d36f
Rashmi Suri <rashmi.suri@avanade.com>
2022-05-23 12:55:27 +0100

    Create implemetation for ICrudRepository interface

---
+++ LibraryLog/Data/Repositories/DescriptionRepository.cs
@@ -0,0 +1,44 @@
+﻿using LibraryLog.Models;
+
+namespace LibraryLog.Data.Repositories
+{
+    public class DescriptionRepository : ICrudRepository<BookDescription, int>
+    {
+        private readonly DescriptionContext _DescriptionContext;
+        public DescriptionRepository(DescriptionContext DescriptionContext)
+        {
+            _DescriptionContext = DescriptionContext ?? throw new
+            ArgumentNullException(nameof(DescriptionContext));
+        }
+        public void Add(BookDescription element)
+        {
+            _DescriptionContext.BookDescriptions.Add(element);
+        }
+        public void Delete(int id)
+        {
+            var item = Get(id);
+            if (item is not null) _DescriptionContext.BookDescriptions.Remove(Get(id));
+        }
+        public bool Exists(int id)
+        {
+            return _DescriptionContext.BookDescriptions.Any(u => u.Id == id);
+        }
+        public BookDescription Get(int id)
+        {
+            return _DescriptionContext.BookDescriptions.FirstOrDefault(u => u.Id == id);
+        }
+        public IEnumerable<BookDescription> GetAll()
+        {
+            return _DescriptionContext.BookDescriptions.ToList();
+        }
+        public bool Save()
+        {
+            return _DescriptionContext.SaveChanges() > 0;
+        }
+        public void Update(BookDescription element)
+        {
+            _DescriptionContext.Update(element);
+        }
+    }
+
+}
