a928d74d98d71678f8e32009b311882acb0f55d2
Rashmi Suri <rashmi.suri@avanade.com>
2022-05-24 01:16:44 +0100

    Create author class in Models

---
+++ LibraryLog/Models/Author.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryLog.Models
+{
+    public class Author
+    {
+        [Key]
+        public int AuthorId { get; set; }
+
+        //validation rules which are mandatory
+        [Required(ErrorMessage = "Description is required")]
+        [MaxLength(64, ErrorMessage = "Length of description cannot be greater than 64 characters")]
+        [MinLength(2, ErrorMessage = "Length of description cannot be less than 2 characters")]
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public int? PhoneNumb { get; set; }
+
+    }
+
+}
