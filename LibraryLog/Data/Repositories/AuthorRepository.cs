a7b386d472c36cac9054a45c9358ac3b2002c268
Rashmi Suri <rashmi.suri@avanade.com>
2022-05-24 01:31:56 +0100

    fixed errors

--- LibraryLog/Data/Repositories/AuthorRepository.cs
+++ LibraryLog/Data/Repositories/AuthorRepository.cs
@@ -1,44 +1,43 @@
-﻿using LibraryLog.Models;
+﻿using LibraryLog.Data;
+using LibraryLog.Data.Repositories;
+using LibraryLog.Models;
 
-namespace LibraryLog.Data.Repositories
+public class AuthorRepository : ICrudRepository<Author, int>
 {
-    public class AuthorRepository : ICrudRepository<Book, int>
+    private readonly BookContext _BookContext;
+    public AuthorRepository(BookContext BookContext)
     {
-        private readonly BookContext _BookContext;
-        public AuthorRepository(BookContext BookContext)
-        {
-            _BookContext = BookContext ?? throw new
-            ArgumentNullException(nameof(BookContext));
-        }
-        public void Add(Book element)
-        {
-            _BookContext.Books.Add(element);
-        }
-        public void Delete(int id)
-        {
-            var item = Get(id);
-            if (item is not null) _BookContext.Books.Remove(Get(id));
-        }
-        public bool Exists(int id)
-        {
-            return _BookContext.Books.Any(u => u.Id == id);
-        }
-        public Book Get(int id)
-        {
-            return _BookContext.Books.FirstOrDefault(u => u.Id == id);
-        }
-        public IEnumerable<Book> GetAll()
-        {
-            return _BookContext.Books.ToList();
-        }
-        public bool Save()
-        {
-            return _BookContext.SaveChanges() > 0;
-        }
-        public void Update(Book element)
-        {
-            _BookContext.Update(element);
-        }
+        _BookContext = BookContext ?? throw new
+        ArgumentNullException(nameof(BookContext));
     }
-
+    public void Add(Author element)
+    {
+        _BookContext.Authors.Add(element);
+    }
+    public void Delete(int id)
+    {
+        var item = Get(id);
+        if (item is not null) _BookContext.Authors.Remove(Get(id));
+    }
+    public bool Exists(int id)
+    {
+        return _BookContext.Authors.Any(u => u.AuthorId == id);
+    }
+    public Author Get(int id)
+    {
+        return _BookContext.Authors.FirstOrDefault(u => u.AuthorId == id);
+    }
+    public IEnumerable<Author> GetAll()
+    {
+        return _BookContext.Authors.ToList();
+    }
+    public bool Save()
+    {
+        return _BookContext.SaveChanges() > 0;
+    }
+    public void Update(Author element)
+    {
+        _BookContext.Update(element);
+    }
 }
+
